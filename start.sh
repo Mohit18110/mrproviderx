c0312b9255612dfbf6853c79e3930a3edc35b88c
Anonymous-18110 <100400779+Anonymous-18110@users.noreply.github.com>
2022-03-01 23:40:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/josprojects/tgmoviebot.git /tgmoviebot
-cd /tgmoviebot
+git clone -b main https://github.com/Anonymous-18110/mrproviderx.git /mrproviderx
+cd /mrproviderx
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
